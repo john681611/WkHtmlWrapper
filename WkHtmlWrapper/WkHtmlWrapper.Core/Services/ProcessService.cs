275229a8782f44ec8f76f0097163a995e6240095
john681611 <john681611@hotmail.com>
2022-03-09 17:47:22 +0000

    Added exception throwing to process logic

--- WkHtmlWrapper/WkHtmlWrapper.Core/Services/ProcessService.cs
+++ WkHtmlWrapper/WkHtmlWrapper.Core/Services/ProcessService.cs
@@ -15,12 +15,18 @@ namespace WkHtmlWrapper.Core.Services
                     StartInfo = new ProcessStartInfo(filename, arguments)
                     {
                         CreateNoWindow = true,
-                        WindowStyle = ProcessWindowStyle.Hidden
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true
                     }
                 };
 
                 process.Start();
                 process.WaitForExit();
+                var logs = process.StandardOutput.ReadToEnd();
+                var errors = process.StandardError.ReadToEnd();
+                if(!string.IsNullOrEmpty(errors))
+                    throw new System.Exception($"Process failed with: {errors} \n\n Logs:{logs}");
             });
         }
     }
